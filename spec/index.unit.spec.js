a8aeec153b02a65d97fb54a704d09a7bd84b6538
lykmapipo <lallyelias87@gmail.com>
2016-09-21 14:15:55 +0300

    initial spec

--- spec/index.unit.spec.js
+++ spec/index.unit.spec.js
@@ -1,1 +1,13 @@
 'use strict';
+
+//dependencies
+const path = require('path');
+const expect = require('chai').expect;
+const wallet = require(path.join(__dirname, '..'))();
+
+describe('wallet', function () {
+  it('should be exportable', function () {
+    expect(wallet).to.exist;
+    expect(wallet).to.be.an.Object;
+  });
+});
